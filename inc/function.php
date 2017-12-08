86e0c17bd5348f3b35da95851fad39755be6aba4
VICONTE <jbviconte.@live.fr>
2017-12-08 14:59:11 +0100

    first commit

--- inc/function.php
+++ inc/function.php
@@ -1,5 +1,6 @@
 <?php
 
+// function connection et déconnetion visible/non-visible
 function connecLogin(){
   if(!empty($_SESSION)) {
     if (!empty($_SESSION['user']['pseudo']) && !empty($_SESSION['user']['id']) && is_numeric($_SESSION['user']['id']) && !empty($_SESSION['user']['role']) && !empty($_SESSION['user']['ip']) && $_SESSION['user']['ip'] == $_SERVER['REMOTE_ADDR']) {
