2cd13b24828e793b1fe2c6112b52d4a18e40b9f4
VICONTE <jbviconte.@live.fr>
2017-12-12 09:23:37 +0100

    third commit

--- index.php
+++ index.php
@@ -56,8 +56,7 @@ $sql = "SELECT * FROM movies_full ORDER BY RAND() LIMIT 100";
     <label><input type="checkbox" name="check" value="western" />Western</label><br />
     <label><input type="checkbox" name="check" value="amour" />Romance</label>
     <br />
-    <button id = "bouton" value="1" onclick="checkUncheckALL();" >Tout cocher</button>
-
+    <button id="bouton" value="1" onclick="checkUncheckALL();" >Tout cocher</button>
 
 </div><br /><br />
 
